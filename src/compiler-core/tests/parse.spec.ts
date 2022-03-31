14546253e35f91fbbfa26e1ec686f49db5b23b7b
TTiip <2711430287@qq.com>
2022-04-01 00:49:52 +0800

    feat: 实现解析三种联合类型

--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -89,6 +89,45 @@ describe('Parse', () => {
       })
     })
 
+    test('Nested element ~~~ ', () => {
+      const ast = baseParse('<div><p>hi</p>{{message}} -- {{message11}}</div>');
+
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.ELEMENT,
+        tag: 'div',
+        children: [
+          {
+            type: NodeTypes.ELEMENT,
+            tag: 'p',
+            children: [
+              {
+                type: NodeTypes.TEXT,
+                content: 'hi'
+              }
+            ]
+          },
+          {
+            type: NodeTypes.INTERPOLATION,
+            content: {
+              type: NodeTypes.SIMPLE_EXPRESSION,
+              content: 'message'
+            }
+          },
+          {
+            type: NodeTypes.TEXT,
+            content: ' -- '
+          },
+          {
+            type: NodeTypes.INTERPOLATION,
+            content: {
+              type: NodeTypes.SIMPLE_EXPRESSION,
+              content: 'message11'
+            }
+          }
+        ]
+      })
+    })
+
     test('should throw error when lack end tag', () => {
       expect(() => {
         baseParse('<div><span></div>')
