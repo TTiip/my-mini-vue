24dc677d200458d75a38754436a90c607d9b4083
TTiip <2711430287@qq.com>
2022-04-05 04:13:09 +0800

    feat: 实现编译 template 成 render 函数

--- example/compiler-base/App.js
+++ example/compiler-base/App.js
@@ -1,12 +1,12 @@
-import { ref } from "../../lib/guide-mini-vue.esm.js";
+import { ref } from '../../lib/guide-mini-vue.esm.js'
 
 export const App = {
-  name: "App",
+  name: 'App',
   template: `<div>hi, {{count}}</div>`,
   setup() {
     const count = (window.count = ref(1));
     return {
-      count,
-    };
-  },
-};
+      count
+    }
+  }
+}
--- example/compiler-base/main.js
+++ example/compiler-base/main.js
@@ -1,5 +1,5 @@
-import { createApp } from "../../lib/guide-mini-vue.esm.js";
-import { App } from "./App.js";
+import { createApp } from '../../lib/guide-mini-vue.esm.js'
+import { App } from './App.js'
 
-const rootContainer = document.querySelector("#app");
-createApp(App).mount(rootContainer);
+const rootContainer = document.querySelector('#app')
+createApp(App).mount(rootContainer)
